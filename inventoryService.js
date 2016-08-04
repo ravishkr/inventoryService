d7e8e8cbe03ddd6cf80c1aeb93d769222e8dcff5
Ravish <ravish@localhost.localdomain>
2016-08-04 14:14:58 +0530

    redis code

---
+++ inventoryService.js
@@ -0,0 +1,12 @@
+var redis = require ('redis'); 
+var client = redis.createClient (); 
+
+client.on ('connect', function() {
+	console.log ('connected');
+}); 
+
+client.set ("books_count","123",redis.print);
+client.set ("books_count",function (err,reply) {
+	console.log('books_count = ' + reply.toString());
+});
+
